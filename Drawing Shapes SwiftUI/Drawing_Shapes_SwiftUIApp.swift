bb225aaf159533de2d6ce3f37651fb7283e42279
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 14:55:32 +0200

    Initial Commit

---
+++ Drawing Shapes SwiftUI/Drawing_Shapes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_Shapes_SwiftUIApp.swift
+//  Drawing Shapes SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_Shapes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
