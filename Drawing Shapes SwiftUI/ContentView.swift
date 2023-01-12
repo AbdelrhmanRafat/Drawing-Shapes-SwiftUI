55727b154184ff19689b2358ff993145925552ce
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-12 14:18:28 +0200

    Adding Offset and overlay on Pie Chart.

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -11,11 +11,41 @@ struct ContentView: View {
     var body: some View {
         //Pie Chart.
         ZStack {
-            ArcView(startAngle: .zero, endAngle: Angle(degrees: 190.0), ArcColor: .init(.systemYellow))
-            //The End Angle is the Start Angle for the next Arc.
-            ArcView(startAngle: Angle(degrees: 190.0), endAngle: Angle(degrees: 110), ArcColor: .init(.systemTeal))
-            ArcView(startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), ArcColor: .blue)
-            ArcView(startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), ArcColor: .purple)
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 190.0), clockwise: true)
+            }
+            .fill(Color.init(.systemYellow))
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
+            }
+            .fill(Color.init(.systemTeal))
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
+            }
+            .fill(Color.blue)
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360.0), clockwise: true)
+            }
+            .fill(Color.purple)
+            .offset(x: 20, y: 20) // Offset the position of Chart.
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360.0), clockwise: true)
+                path.closeSubpath()
+            }
+            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10) // Add Border to Chart.with same offset
+            .offset(x: 20, y: 20)
+            .overlay( // OverLay Text on Chart
+                 Text("25%")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .offset(x: 80, y: -120) // Offset Position of Text to be inside Chart.
+            )
         }
     }
 }
@@ -23,19 +53,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct ArcView: View {
-    var startAngle : Angle
-    var endAngle : Angle
-    var ArcColor : Color
-    var body: some View {
-        Path() { path in
-            path.move(to: CGPoint(x: 200, y: 200))
-            path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            
-        }
-        .fill(ArcColor)
     }
 }
