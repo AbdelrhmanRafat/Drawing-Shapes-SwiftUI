78e5a2b2c9b86a88cb71998cc8024f2b8ce0705b
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-12 11:50:42 +0200

    ‘Drawing_Rectangle_Using_Path’

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //Path is struct Containing the outline of 2d Shape.
+        Path() { path in
+            path.move(to: CGPoint(x: 20, y: 20))//Move the pointer to be (20,20)Starting Point.
+            path.addLine(to: CGPoint(x: 300, y: 20))// Draw line from (20,20)'Starting point' to (300,20)
+            path.addLine(to: CGPoint(x: 300, y: 200))// Draw line from (3000,20)'last point' to (300,200)
+            path.addLine(to: CGPoint(x: 20, y: 200))
+            // Draw line from (3000,200)'last point' to (20,200)
+        }
+        .fill(Color.green) // Fill Shape with Color.
     }
 }
 
