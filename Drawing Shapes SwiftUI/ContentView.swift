23973f6e27ff7e9bf49bc042bf728c52cec11841
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-12 15:59:15 +0200

    Refactor Pie-Chart Code.

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        BuiltInShapes()
+        //Organize Pie Chart Code Using Shape Protocol.
+        ZStack {
+            ChartView(startAngle: 0, endAngle: 190.0).fill(Color.init(.systemYellow))
+            ChartView(startAngle: 190.0, endAngle: 110.0) .fill(Color.init(.systemTeal))
+            ChartView(startAngle: 110.0, endAngle: 90.0).fill(Color.blue)
+            ChartView(startAngle: 90.0, endAngle: 360.0)
+            .fill(Color.purple)
+            .offset(x: 20, y: 20) // Offset the position of Chart.
+            ChartView(startAngle: 90.0, endAngle: 360.0)
+            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10) // Add Border to Chart.with same offset
+            .offset(x: 20, y: 20)
+            .overlay( // OverLay Text on Chart
+                 Text("25%")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .offset(x: 80, y: -120) // Offset Position of Text to be inside Chart.
+            )
+        }
     }
 }
 
@@ -18,17 +36,16 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-//Introducing Some Built in Shapes.
-struct BuiltInShapes : View {
-    var body: some View {
-        Circle()
-            .foregroundColor(.green)
-            .frame(width: 200, height: 200, alignment: .center)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 80, height: 80, alignment: .center)
-                    .foregroundColor(.white)
-            )
+struct ChartView : Shape {
+    var startAngle : Double
+    var endAngle : Double
+    func path(in rect: CGRect) -> Path {
+        let path = Path { path in
+            path.move(to: CGPoint(x: 200, y: 200))
+            path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: startAngle), endAngle: Angle(degrees: endAngle), clockwise: true)
+            path.closeSubpath()
+        }
+        return path
     }
+    
 }
