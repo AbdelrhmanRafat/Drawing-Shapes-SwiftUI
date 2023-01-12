1ec6b341477ed383729e8a4a3dd7963143d74833
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-12 14:33:54 +0200

    Exercise on Path Shapes.

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -9,44 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //Pie Chart.
-        ZStack {
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 190.0), clockwise: true)
-            }
-            .fill(Color.init(.systemYellow))
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
-            }
-            .fill(Color.init(.systemTeal))
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
-            }
-            .fill(Color.blue)
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360.0), clockwise: true)
-            }
-            .fill(Color.purple)
-            .offset(x: 20, y: 20) // Offset the position of Chart.
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360.0), clockwise: true)
-                path.closeSubpath()
-            }
-            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10) // Add Border to Chart.with same offset
-            .offset(x: 20, y: 20)
-            .overlay( // OverLay Text on Chart
-                 Text("25%")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .bold()
-                    .foregroundColor(.white)
-                    .offset(x: 80, y: -120) // Offset Position of Text to be inside Chart.
-            )
+        Path { path in
+            path.move(to: CGPoint(x: 20, y: 20))
+            path.addQuadCurve(to: CGPoint(x: 220, y: 20), control: CGPoint(x: 110, y: 0))
+            //Control Point (x) is in the middle
+            //(y) 20 points higher Try Change value of (y) to be positive and negative numbers to observe the change.
+            path.addLine(to: CGPoint(x: 220, y: 60))
+            path.addLine(to: CGPoint(x: 20, y: 60))
         }
+        .fill(Color.green)
     }
 }
 
