e66dd903cb994ae95b881d0083721de71d96d370
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-14 12:10:50 +0200

    Creating Donut Chart.

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -8,31 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var purpleGradient = LinearGradient(gradient: Gradient(colors: [Color("LightPurple"), Color("DarkPurple")]), startPoint: .trailing, endPoint: .leading)
     var body: some View {
-        //Creating Progress Indicator Using Shapes.
+        //Creating Donut Chart Using Shapes.
         ZStack {
             Circle()
-                .stroke(Color(.systemGray6),lineWidth: 20)// Use Stroke Modifier to draw outline of circle.
-                .frame(width: 300, height: 300, alignment: .center)
+                .trim(from: 0.0, to: 0.4)// Using Trim To Create Donut Chart.
+                .stroke(Color(.systemBlue),lineWidth: 80)
             Circle()
-                .trim(from: 0.0, to: 0.85)// Trim Modifier is the trick to create an open Circle.
-                .stroke(purpleGradient,lineWidth: 20)//The order is very important as the trim comes first then stroke then Frame.
-                .frame(width: 300, height: 300, alignment: .center)
+                .trim(from: 0.4, to: 0.6)
+                .stroke(Color(.systemTeal),lineWidth: 80)
+            Circle()
+                .trim(from: 0.6, to: 0.75)
+                .stroke(Color(.systemPurple),lineWidth: 80)
+            Circle()
+                .trim(from: 0.75, to: 1)
+                .stroke(Color(.systemYellow),lineWidth: 90)
                 .overlay(
-                    VStack {
-                        Text("85%")
-                        .font(.system(size: 80, weight: .bold, design: .rounded))
-                            .foregroundColor(Color(.systemGray))
-                        Text("Complete")
-                            .font(.system(.body, design: .rounded))
-                            .bold()
-                            .foregroundColor(Color(.systemGray))
-                    }
-                
+                Text("25%")
+                    .font(.system(.title, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .offset(x: 120, y: -100)
                 )
-                
         }
+        .frame(width: 300, height: 300, alignment: .center)
     }
 }
 
