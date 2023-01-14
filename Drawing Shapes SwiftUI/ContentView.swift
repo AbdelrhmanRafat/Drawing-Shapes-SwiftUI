0e0a56687ff53d64331c081061a10ea898a8fa2a
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-14 11:49:50 +0200

    Creating Progress Indicator

--- Drawing Shapes SwiftUI/ContentView.swift
+++ Drawing Shapes SwiftUI/ContentView.swift
@@ -8,25 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var purpleGradient = LinearGradient(gradient: Gradient(colors: [Color("LightPurple"), Color("DarkPurple")]), startPoint: .trailing, endPoint: .leading)
     var body: some View {
-        //Organize Pie Chart Code Using Shape Protocol.
+        //Creating Progress Indicator Using Shapes.
         ZStack {
-            ChartView(startAngle: 0, endAngle: 190.0).fill(Color.init(.systemYellow))
-            ChartView(startAngle: 190.0, endAngle: 110.0) .fill(Color.init(.systemTeal))
-            ChartView(startAngle: 110.0, endAngle: 90.0).fill(Color.blue)
-            ChartView(startAngle: 90.0, endAngle: 360.0)
-            .fill(Color.purple)
-            .offset(x: 20, y: 20) // Offset the position of Chart.
-            ChartView(startAngle: 90.0, endAngle: 360.0)
-            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10) // Add Border to Chart.with same offset
-            .offset(x: 20, y: 20)
-            .overlay( // OverLay Text on Chart
-                 Text("25%")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .bold()
-                    .foregroundColor(.white)
-                    .offset(x: 80, y: -120) // Offset Position of Text to be inside Chart.
-            )
+            Circle()
+                .stroke(Color(.systemGray6),lineWidth: 20)// Use Stroke Modifier to draw outline of circle.
+                .frame(width: 300, height: 300, alignment: .center)
+            Circle()
+                .trim(from: 0.0, to: 0.85)// Trim Modifier is the trick to create an open Circle.
+                .stroke(purpleGradient,lineWidth: 20)//The order is very important as the trim comes first then stroke then Frame.
+                .frame(width: 300, height: 300, alignment: .center)
+                .overlay(
+                    VStack {
+                        Text("85%")
+                        .font(.system(size: 80, weight: .bold, design: .rounded))
+                            .foregroundColor(Color(.systemGray))
+                        Text("Complete")
+                            .font(.system(.body, design: .rounded))
+                            .bold()
+                            .foregroundColor(Color(.systemGray))
+                    }
+                
+                )
+                
         }
     }
 }
@@ -35,17 +40,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-struct ChartView : Shape {
-    var startAngle : Double
-    var endAngle : Double
-    func path(in rect: CGRect) -> Path {
-        let path = Path { path in
-            path.move(to: CGPoint(x: 200, y: 200))
-            path.addArc(center: CGPoint(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: startAngle), endAngle: Angle(degrees: endAngle), clockwise: true)
-            path.closeSubpath()
-        }
-        return path
-    }
-    
 }
